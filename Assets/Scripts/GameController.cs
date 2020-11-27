72cc720484ae2027dbc635152ae1fbefe1cf7687
martahil <marta.hil.1988@gmail.com>
2020-11-26 22:40:27 -0300

    Created a simple menu
    * Code, assets and gameobjects cleaning;
    * Gameplay prefab was created;
    * Created a simple menu;

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,15 +12,58 @@ public class GameController : MonoBehaviour
     public AudioSource point_sfx;
     public float fadeTime = 1;
     public AudioSource music;
+
+    public GameObject menu;
+    public GameObject firstTapButton;
+
+    public GameObject jumpButton;
+    public GameObject spawnVacuums;
+
+    public GameObject gameOverPanel;
+
+    private PlayerController playerController;
+
+     
+
+
     void Start()
     {
-        Time.timeScale = 1;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        
         point_sfx = GetComponent<AudioSource>();
         music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
     }
+
+    public void StartGame()
+    {
+        menu.SetActive(false);
+        firstTapButton.SetActive(true);
+    }
+    
+    public void FirstTap()
+    {
+        firstTapButton.SetActive(false);
+        playerController.GetComponent<Rigidbody2D>().simulated = true;
+        playerController.Jump();
+
+        jumpButton.SetActive(true);
+
+        spawnVacuums.SetActive(true);        
+
+    }
+
+    public void GameOver()
+    {
+        StartCoroutine(FadeAudioSource.StartFade(music, 3f, 0f));
+        gameOverPanel.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
         GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().volume = 1f;
     }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,35 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
     private Rigidbody2D rig;
-    private AudioSource jump_sfx;
-    public GameObject GameOver;
-    // private GameController gameController;
-
+    private AudioSource jump_sfx;    
+    private GameController gameController;
     private AudioSource music;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        jump_sfx = GetComponent<AudioSource>();
-        // gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        //jump_sfx = GetComponent<AudioSource>();
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
         music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (!GameOver.activeSelf
-        && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            rig.velocity = Vector2.up * speed;
-            jump_sfx.Play();
+            Jump();
         }
+        
     }
 
+    public void Jump()
+    {
+        rig.velocity = Vector2.up * speed;
+    }
     void OnCollisionEnter2D(Collision2D colisor)
     {
         //  GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().volume = 0.2f;
-        StartCoroutine(FadeAudioSource.StartFade(music, 3f, 0f));
-        GameOver.SetActive(true);
-        Time.timeScale = 0;
+        gameController.GameOver();
+        
     }
 }
