7851bf7719b94f5ef23817bf80a17e8866340c13
Augusto Polonio <augustodrp@gmail.com>
2020-11-29 11:22:24 -0300

    Visual Improvements
    * Added "Confortaa" font;
    * Added UI animations;
    * Black Cat player was modified

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,9 @@ public class GameController : MonoBehaviour
     [Header("=== UI ===")]
     public int Score;
     public Text scoreText;
+    public Text gameplayText;
     public GameObject menu;
+    public GameObject PlayButton;
     public GameObject firstTapButton;
     public GameObject jumpButton;
     public GameObject spawnVacuums;
@@ -24,14 +26,51 @@ public class GameController : MonoBehaviour
     {
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         
+        Time.timeScale = 1;
+
         point_sfx = GetComponent<AudioSource>();
         music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+
+        ShowMenu();
     }
 
     public void StartGame()
     {
-        menu.SetActive(false);
+        //menu.SetActive(false);        
+        //menu.GetComponent<Animator>().Play("FadeOut_up_to_down");
+        HideMenu();
         firstTapButton.SetActive(true);
+        gameplayText.gameObject.SetActive(true);        
+        gameplayText.GetComponent<Animator>().Play("FadeIn_down_to_up");
+
+        playerController.showingPlayer = true;
+    }
+
+    private void ShowMenu()
+    {
+        foreach (Transform buttons in menu.transform)
+        {
+            buttons.GetComponent<Animator>().Play("FadeIn");
+        }
+    }
+
+    private void HideMenu()
+    {
+        menu.GetComponent<Animator>().Play("FadeOut_up_to_down");
+
+        foreach (Transform buttons in menu.transform)
+        {
+            if (buttons.GetComponent<Button>() != null)
+                buttons.GetComponent<Button>().interactable = false;
+
+            buttons.GetComponent<Animator>().Play("FadeOut");
+            buttons.GetComponent<Animator>().speed = 3f;
+        }
+    }
+
+    private void FadeOutTextAfterTime()
+    {
+        gameplayText.GetComponent<Animator>().Play("FadeOut");
     }
     
     public void FirstTap()
@@ -41,15 +80,29 @@ public class GameController : MonoBehaviour
         playerController.Jump();
 
         jumpButton.SetActive(true);
-
         spawnVacuums.SetActive(true);        
+        scoreText.gameObject.SetActive(true);        
 
+        scoreText.GetComponent<Animator>().Play("FadeIn");
+
+        if (gameplayText.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("FadeIn_down_to_up"))
+        {
+            //If animation to show text still running, run FadeOut after time
+            print("If animation to show text still running, run FadeOut after time");
+            Invoke("FadeOutTextAfterTime", 1.5f);
+            gameplayText.GetComponent<Animator>().speed = 1.5f;
+        }
+        else
+        {
+            gameplayText.GetComponent<Animator>().Play("FadeOut");
+        }
     }
 
     public void GameOver()
     {
         StartCoroutine(FadeAudioSource.StartFade(music, 3f, 0f));
         gameOverPanel.SetActive(true);
+        jumpButton.SetActive(false);
 
         Time.timeScale = 0;
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
     private Rigidbody2D rig;
-    private AudioSource jump_sfx;    
+    private AudioSource jump_sfx;
     private GameController gameController;
     private AudioSource music;
+    public bool showingPlayer;
+    public Transform targetToMovePlayer;
 
     void Start()
     {
@@ -16,15 +18,47 @@ public class PlayerController : MonoBehaviour
         //jump_sfx = GetComponent<AudioSource>();
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
         music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+
+        //targetToMovePlayer = new Vector3(0, transform.position.y, transform.position.z);
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-        
+    }
+
+    void FixedUpdate()
+    {
+        if (showingPlayer)
+        {
+            // transform.position = Vector3.MoveTowards(transform.position, targetToMovePlayer.position, 0.5f * Time.deltaTime);
+
+            // //Check if the position of the cube and sphere are approximately equal. (When done..)
+            // if (Vector3.Distance(transform.position, targetToMovePlayer.position) < 0.001f)
+            // { 
+            //     showingPlayer = false;
+            // }
+            //transform.position += Vector3.right * Time.deltaTime;
+            //transform.position += new Vector3(3f * Time.deltaTime , 0, 0);
+
+            
+
+            transform.position = Vector3.Lerp(transform.position, targetToMovePlayer.position, 0.8f * Time.deltaTime);
+
+            //Check if the position of the cube and sphere are approximately equal. (When done..)
+            if (Vector3.Distance(transform.position, targetToMovePlayer.position) < 0.001f)
+            { 
+                showingPlayer = false;
+            }
+
+            //transform.position = Vector3.Lerp(transform.position, targetToMovePlayer, Time.deltaTime);
+
+        //     Vector3 newPos = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+        //  transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 1f);
+        }
     }
 
     public void Jump()
@@ -35,6 +69,6 @@ public class PlayerController : MonoBehaviour
     {
         //  GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().volume = 0.2f;
         gameController.GameOver();
-        
+
     }
 }
