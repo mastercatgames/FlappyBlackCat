7463fc8559dea515f814c26d05010191e1d9c6bc
Augusto Polonio <augustodrp@gmail.com>
2020-11-29 18:55:44 -0300

    GameOver panel animations

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     private PlayerController playerController;
+    private SpawnPipes spawnPipes;
+
     [Header("=== UI ===")]
     public int Score;
     public Text scoreText;
@@ -18,15 +20,18 @@ public class GameController : MonoBehaviour
     public GameObject firstTapButton;
     public GameObject jumpButton;
     public GameObject spawnVacuums;
-    public GameObject gameOverPanel;    
+    public GameObject gameOverPanel; 
+
     [Header("=== Audio ===")]
     public AudioSource point_sfx;
     public AudioSource music;
     public float fadeTime = 1;
     public List<string> readyRamdomTexts;
+    
     void Start()
     {
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        spawnPipes = GameObject.FindGameObjectWithTag("MainCamera").transform.Find("SpawnVacuums").GetComponent<SpawnPipes>();
         
         Time.timeScale = 1;
 
@@ -42,7 +47,7 @@ public class GameController : MonoBehaviour
         readyRamdomTexts = new List<string>();
         readyRamdomTexts.Add("Enjoy");
         readyRamdomTexts.Add("Thanks for playing");
-    }
+    }    
 
     public void StartGame()
     {
@@ -130,14 +135,63 @@ public class GameController : MonoBehaviour
         StartCoroutine(FadeAudioSource.StartFade(music, 3f, 0f));
         gameOverPanel.SetActive(true);
         jumpButton.SetActive(false);
+        //scoreText.gameObject.SetActive(false);
+        scoreText.GetComponent<Animator>().Play("FadeOut");
+        scoreText.GetComponent<Animator>().speed = 3f;
 
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
+
+        spawnPipes.CancelInvoke();
+
+        //Player
+        playerController.transform.parent = null;
+        StartCoroutine(SetActiveAfterTime(playerController.gameObject, false, 2.5f));
+
+        // float delayToShow = 1f;
+        //Hide All objects in panel
+        // foreach (Transform child in gameOverPanel.transform)
+        // {
+        //     // child.gameObject.SetActive(false);
+        //     // if (child.gameObject.name.Contains("Best"))
+        //     //     delayToShow = 1.5f;
+        //     // else if (child.gameObject.name.Contains("ContinueButton"))
+        //     //     delayToShow = 2f;
+        //     // else if (child.gameObject.name.Contains("RetryButton"))
+        //     //     delayToShow = 4f;
+
+        //     StartCoroutine(SetActiveAfterTime(child.gameObject, true, 0.5f/*delayToShow*/));
+        // }
+
+        gameOverPanel.transform.Find("ScoreNum").Find("Text").GetComponent<Text>().text = scoreText.text;
+
+        //Activate objects smoothly
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("Score").gameObject, true, 0.2f));
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("ScoreNum").gameObject, true, 0.2f));
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("Best").gameObject, true, 0.5f));
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("BestNum").gameObject, true, 0.5f));
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("ContinueButton").gameObject, true, 0.8f));
+        StartCoroutine(SetActiveAfterTime(gameOverPanel.transform.Find("RetryButton").gameObject, true, 3f));
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
         GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().volume = 1f;
+    }
+
+    public IEnumerator SetActiveAfterTime(GameObject gameObject, bool active, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(active);
+    }
+
+    public IEnumerator PlayAnimationAfterTime(Animator animator, string animationName, float delay, float speed = 0)
+    {
+        yield return new WaitForSeconds(delay);
+        animator.Play(animationName);
+        if (speed > 0)
+            animator.speed = speed;
+        print("Finish Animation!");
     }
 
 }
