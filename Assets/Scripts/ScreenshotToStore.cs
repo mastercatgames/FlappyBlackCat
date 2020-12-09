5b1fcca424b542d2f12ee2b30251be471ffedcb8
Augusto Polonio <augustodrp@gmail.com>
2020-12-09 01:29:15 -0300

    Some improvements
    * Created a scene just to capture screenshots;
    * Created different materials to different colors;

---
+++ Assets/Scripts/ScreenshotToStore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotToStore : MonoBehaviour
+{
+    string saveLocation = "/Documents/_Game Dev/_Arquivos de projeto/Flappy Black Cat (arquivos diversos)/Screenshots for store/";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Press P to take a screenshot.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            string file = saveLocation + Screen.width + "x" + Screen.height + "-" + timestamp + ".png";
+            ScreenCapture.CaptureScreenshot(file);
+            print("Screenshot saved at " + file);
+        }
+    }
+}
